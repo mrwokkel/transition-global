63e7daf212320be8e9a4474fcf542e1515dda0fb
John van Dijk <john@rubylibre.com>
2024-01-16 15:36:05 +0100

    example

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import { fade } from 'svelte/transition';
+
+  let show = $state(true)
+
+  const { children } = $props<{
+		children: Snippet<void>
+	}>()
+
+
+</script>
+
+<div>
+  <button onclick={() => show = !show}>
+    Toggle
+  </button>
+
+  {#if show}
+    <div transition:fade|global>HELLO</div>
+  {/if}
+</div>
+
+{@render children()}
+
